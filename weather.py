c3a1309d7ab1ab2405f9410f96bbad78d2cf479e
Bryan Ng <bryanngcy@gmail.com>
2024-01-19 15:29:30 +0800

    Update weather.py
    added print(weather)

--- weather.py
+++ weather.py
@@ -14,6 +14,7 @@ def get_weather(city):
     result = sock.read()
     sock.close()
     weather = json.loads(result)
+    print(weather)
     return weather["main"]["temp"] - 273.15
 
 def postal_lookup(postal_code):
